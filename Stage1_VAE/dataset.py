3f84bb36a3967298984870dacb8ab5e768f9a048
nhaquynh <quynhnhaa@gmail.com>
2025-10-06 09:04:22 +0700

    add evaluate.py gemini

--- Stage1_VAE/dataset.py
+++ Stage1_VAE/dataset.py
@@ -234,8 +234,7 @@ class BratsDataset(Dataset):
 
     def __getitem__(self, index):
         patient = self.patient_names[index]
-        # self.file_path = os.path.join(self.data_path, patient + ".npy")
-        self.file_path = os.path.join('/kaggle/input/data-npy2', patient + ".npy")
+        self.file_path = os.path.join(self.data_path, patient + ".npy")
         if self.phase == "test":
             # self.file_path = os.path.join(self.test_path, 'npy', patient + ".npy")
             self.file_path = os.path.join('/kaggle/input/data-npy2', patient + ".npy")

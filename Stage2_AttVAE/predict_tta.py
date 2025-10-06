fb3cfe16cd8787b171e0328b9a184265b4765b8d
nhaquynh <quynhnhaa@gmail.com>
2025-10-06 10:33:23 +0700

    chay predict stage2

--- Stage2_AttVAE/predict_tta.py
+++ Stage2_AttVAE/predict_tta.py
@@ -101,7 +101,7 @@ def init_model_from_states(config):
             model = torch.nn.DataParallel(model)   # multi-gpu inference
         model = model.cuda()
     checkpoint = torch.load(config['saved_model_path'], map_location='cpu', weights_only=False)
-    state_dict = checkpoint["state_dict"]
+    state_dict = checkpoint.state_dict()
     if not config["load_from_data_parallel"]:
         model.load_state_dict(state_dict)
     else:
